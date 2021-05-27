0eaa3efca65f26c08ae52a1ccf3d536cda5d2524
假面骑士龙骑 <332181764@qq.com>
2021-05-27 23:51:25 +0800

    Add files via upload

---
+++ Basketball/frmMain.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace Basketball
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.picBoard = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoard)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBoard
+            // 
+            this.picBoard.Image = ((System.Drawing.Image)(resources.GetObject("picBoard.Image")));
+            this.picBoard.Location = new System.Drawing.Point(12, 12);
+            this.picBoard.Name = "picBoard";
+            this.picBoard.Size = new System.Drawing.Size(600, 400);
+            this.picBoard.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBoard.TabIndex = 0;
+            this.picBoard.TabStop = false;
+            this.picBoard.Paint += new System.Windows.Forms.PaintEventHandler(this.picBoard_Paint);
+            this.picBoard.MouseDown += new System.Windows.Forms.MouseEventHandler(this.picBoard_MouseDown);
+            this.picBoard.MouseMove += new System.Windows.Forms.MouseEventHandler(this.picBoard_MouseMove);
+            this.picBoard.MouseUp += new System.Windows.Forms.MouseEventHandler(this.picBoard_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(632, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "重    置";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblResult.Location = new System.Drawing.Point(632, 76);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 24);
+            this.lblResult.TabIndex = 2;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(755, 427);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.picBoard);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "篮球入框功能验证";
+            ((System.ComponentModel.ISupportInitialize)(this.picBoard)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBoard;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ Basketball/frmMain.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Basketball
+{
+    public partial class frmMain : Form
+    {
+        private Random m_random = new Random();
+        private Rectangle m_basketryBox = new Rectangle(0, 0, 50, 50);
+        private Rectangle m_basketballBox=new Rectangle(0,0,30,30);
+
+        private void ResetPosition()
+        {
+            m_basketryBox.X = m_random.Next(picBoard.Width - m_basketryBox.Width);
+            m_basketryBox.Y = m_random.Next(picBoard.Height - m_basketryBox.Height);
+
+            do
+            {
+                m_basketballBox.X = m_random.Next(picBoard.Width - m_basketballBox.Width);
+                m_basketballBox.Y = m_random.Next(picBoard.Height - m_basketballBox.Height);
+            } while (m_basketballBox.IntersectsWith(m_basketryBox)); 
+        }
+
+
+        public frmMain()
+        {
+            InitializeComponent();
+
+            ResetPosition();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ResetPosition();
+            lblResult.Text = string.Empty;
+            picBoard.Refresh();
+        }
+
+        private void picBoard_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(Basketball.Properties.Resources.basketry, m_basketryBox);
+            e.Graphics.DrawImage(Basketball.Properties.Resources.basketball, m_basketballBox);
+        }
+
+        private bool m_dragging=false;
+        private Point m_prePosition = new Point();
+
+        private void picBoard_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left &&
+                m_basketballBox.Contains(e.Location))
+            {
+                m_dragging = true;
+                m_prePosition = e.Location;
+            }
+        }
+
+        private void picBoard_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (m_dragging)
+            {
+                m_basketballBox.X += e.Location.X - m_prePosition.X;
+                m_basketballBox.Y += e.Location.Y - m_prePosition.Y;
+
+                m_prePosition = e.Location;
+
+                picBoard.Refresh();
+            }
+        }
+
+        private void picBoard_MouseUp(object sender, MouseEventArgs e)
+        {
+            m_dragging = false;
+
+            if (m_basketryBox.Contains(m_basketballBox))
+            {
+                lblResult.ForeColor = Color.Green;
+                lblResult.Text = "太棒了";
+            }
+            else
+            {
+                lblResult.ForeColor = Color.Red;
+                lblResult.Text = "可惜了";
+            }
+        }
+    }
+}
